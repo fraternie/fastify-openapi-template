1c3172be6bdbb5fdcf4648fc9fadf318ca7041e0
sarcastic-verma <shivamthegreat.sv@gmail.com>
2021-07-13 02:11:45 +0530

    :bug: Added missing file

---
+++ src/services/base.service.ts
@@ -0,0 +1,11 @@
+export class BaseService {
+    // static baseReply(req: FastifyRequest): BaseServiceReply {
+    //   return {
+    //     notifyUser: false,
+    //     message: 'Success',
+    //     code: 200,
+    //     data: [],
+    //     error: null,
+    //   };
+    // }
+}
